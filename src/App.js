7521e3ed89c356320acb86acf1bc08f8f2945339
Mifta Hamid <miftahamid@Miftas-Air.fios-router.home>
2019-03-14 19:10:49 -0400

    Creating a form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Home/Home";
+import Map from "./Maps/Map";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Map />
         <Home />
       </div>
     );
