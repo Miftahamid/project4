7df928dc7603fb15b868cbb46d9cd94421169e52
Mifta Hamid <miftahamid@Miftas-Air.fios-router.home>
2019-03-14 14:39:35 -0400

    adding the map api and home is displaying the content properly

---
+++ src/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Farmer Market</h1>
+        <div>Find your Favorite Farmers Maket.</div>
+      </div>
+    );
+  }
+}
+
+export default Home;
